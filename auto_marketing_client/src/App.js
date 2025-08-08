f4b87c230813d213b49c9b9a590e068bc8574dfa
Vinhnd03 <ducvinhmycanh@gmail.com>
2025-08-08 14:24:37 +0700

    init app

--- auto_marketing_client/src/App.js
+++ auto_marketing_client/src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className='flex justify-center w-full'>
-        <p className="text-red-400">ngu</p>
+        <p className="text-red-400">test</p>
     </div>
     
   );
